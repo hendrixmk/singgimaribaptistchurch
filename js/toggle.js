71a1740f0cabcf2a88b87809f36d98bfa14b6d9a
hendrixmk <42368355+hendrixmk@users.noreply.github.com>
2025-03-31 19:59:57 +0530

    Delete js/toggle.js

--- js/toggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.getElementById("navLinks");
-  document.querySelector(".fa-bars").addEventListener("click", () => {
-    navLinks.style.right = "0";
-  });
-  document.querySelector(".fa-times").addEventListener("click", () => {
-    navLinks.style.right = "-200px";
-  });
-});
